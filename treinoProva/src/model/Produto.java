2418906b4df8daef7044d3301c33f42139d10f28
João Geraldo Bussolo Bevilacqua <joaogeraldobussolo@gmail.com>
2025-06-14 12:00:14 -0300

    14/06

--- treinoProva/src/model/Produto.java
+++ treinoProva/src/model/Produto.java
@@ -1,5 +1,37 @@
 package model;
 
 public class Produto {
-
+	String nome;
+	int qtde;
+	double preco;
+	
+	public Produto(String nome, int qtde, double preco) {
+		this.nome = nome;
+		this.qtde = qtde;
+		this.preco = preco;
+	}
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setQtde(int qtde) {
+		this.qtde = qtde;
+	}
+	
+	public int getQtde() {
+		return qtde;
+	}
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
 }
--- treinoProva/src/model/listaprodutos.java
+++ treinoProva/src/model/listaprodutos.java
@@ -1,5 +1,49 @@
 package model;
+import java.util.ArrayList;
+
+
 
 public class listaprodutos {
-
+	private ArrayList<Produto> produtos;
+	
+	public listaprodutos() {
+		produtos = new ArrayList<>();
+	}
+	
+	public boolean removeProd(String nome) {
+		int p = buscarProduto(nome);
+		if(p >= 0) {
+			produtos.remove(p);
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	
+	public boolean addProd(Produto produto) {
+		String nomeP = produto.getNome();
+		if(buscarProduto(nomeP) == -1){
+			produtos.add(produto);
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public ArrayList<Produto> listarprodutos(){
+		return produtos;
+	}
+		
+	
+	public int buscarProduto(String nome){
+	int i = 0;
+	for(Produto produto : produtos) {
+		if(produto.getNome().equals(nome)) {
+			return i;
+		}
+		return i++;
+	}
+	return -1;
+	}
 }
