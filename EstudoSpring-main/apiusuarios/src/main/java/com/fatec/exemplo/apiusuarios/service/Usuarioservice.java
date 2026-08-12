8cd1ee297ec906e7e9ab0c48fed27debec9c22e5
JoaoGeraldo-dev <joaogeraldobussolo@gmail.com>
2026-08-12 12:21:17 -0300

    12/08

---
+++ EstudoSpring-main/apiusuarios/src/main/java/com/fatec/exemplo/apiusuarios/service/Usuarioservice.java
@@ -0,0 +1,22 @@
+package com.fatec.exemplo.apiusuarios.service;
+import com.fatec.exemplo.apiusuarios.model.Usuario;
+import com.fatec.exemplo.apiusuarios.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+
+public class Usuarioservice {
+@Autowired
+    private UsuarioRepository usuarioRepository;
+public List<Usuario> ListarTodos(){
+        return usuarioRepository.findAll();
+    }
+    public Usuario salvar(Usuario usuario){
+    return usuarioRepository.save(usuario);
+    }
+    public Usuario deletar(Long id){
+        usuarioRepository.deleteById(id);
+
+    }
+}
