b9a7e12d771837ebdcd36ab5684fd631c163804d
João Geraldo Bussolo Bevilacqua <joaogeraldobussolo@gmail.com>
2026-09-04 17:29:46 -0300

    04/09

--- ESTUDOS JAVA DO 0/src/microExerciciosdeLogica/MicroEx.java
+++ ESTUDOS JAVA DO 0/src/microExerciciosdeLogica/MicroEx.java
@@ -4,7 +4,9 @@ public class MicroEx {
     private int temperatura = 20;
     private int combustivel = 40;
     private double saldo = 100;
-
+    private int idade = 18;
+    private boolean ingresso = true;
+    private int velocidade;
 
     public void aumentarTemperatura(int valor){
         if(valor <= 0){
@@ -34,6 +36,27 @@ public class MicroEx {
         }else{
             double total = quantidade * precoUnitario;
             saldo = saldo - total;
+        }
+    }
+
+    public void entrarEvento(int idadePessoa, boolean temIngresso){
+        if (idadePessoa >= 18 && temIngresso){
+            System.out.println("entrada permitida");
+        }else{
+            System.out.println("entrada bloqueada");
+        }
+
+    }
+
+    public void verificarVelocidade(int valor){
+        if(valor < 0){
+            System.out.println("valor invalido");
+        }else if(valor <= 60){
+            System.out.println("velocidade permitida");
+        }else if(valor <= 100){
+            System.out.println("acima do permitido");
+        }else{
+            System.out.println("velocidade muito alta VRUMMMMMMMMMMMMMMMMMMMM");
         }
     }
 
